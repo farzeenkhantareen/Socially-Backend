e214464c70897066e532a5f491b72ba0b882ea50
farzeenkhantareen <farzeenkhan1199@gmail.com>
2025-10-16 20:19:49 +0500

    Profile Picture in Profile page from user database

--- app/src/main/java/com/solocoder/i230721/Profile.kt
+++ app/src/main/java/com/solocoder/i230721/Profile.kt
@@ -2,51 +2,73 @@ package com.solocoder.i230721
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Base64
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import de.hdodenhof.circleimageview.CircleImageView
 
 class Profile : AppCompatActivity() {
+
+    private lateinit var profilePicture: CircleImageView
+    private lateinit var database: DatabaseReference
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_profile)
-        var nav_home=findViewById<ImageView>(R.id.nav_home)
-        nav_home.setOnClickListener {
-            val Intent = Intent(this, Main_feed::class.java)
-            startActivity(Intent)
-            finish()
-        }
-        var nav_search=findViewById<ImageView>(R.id.nav_search)
-        nav_search.setOnClickListener {
-            val Intent = Intent(this, Explore_page::class.java)
-            startActivity(Intent)
-            finish()
-        }
-        var nav_heart=findViewById<ImageView>(R.id.nav_heart)
-        nav_heart.setOnClickListener {
-            val Intent = Intent(this, Notification_You::class.java)
-            startActivity(Intent)
+
+        profilePicture = findViewById(R.id.profile_picture)
+
+        val uid = FirebaseAuth.getInstance().currentUser?.uid ?: return
+        database = FirebaseDatabase.getInstance().getReference("Users").child(uid)
+
+        database.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val base64String = snapshot.child("image").getValue(String::class.java)
+                if (!base64String.isNullOrEmpty()) {
+                    try {
+                        val imageBytes = Base64.decode(base64String, Base64.DEFAULT)
+                        val decodedBitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+                        profilePicture.setImageBitmap(decodedBitmap)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
+
+        findViewById<ImageView>(R.id.nav_home).setOnClickListener {
+            startActivity(Intent(this, Main_feed::class.java))
             finish()
         }
 
-        val edit_page=findViewById<RelativeLayout>(R.id.edit_page)
-        edit_page.setOnClickListener {
-            val Intent = Intent(this, Edit_profile::class.java)
-            startActivity(Intent)
-            finish()
-        }
-        val friends_highlight=findViewById<RelativeLayout>(R.id.friends_highlight)
-        friends_highlight.setOnClickListener {
-            val Intent = Intent(this, Highlight::class.java)
-            startActivity(Intent)
+        findViewById<ImageView>(R.id.nav_search).setOnClickListener {
+            startActivity(Intent(this, Explore_page::class.java))
             finish()
         }
 
+        findViewById<ImageView>(R.id.nav_heart).setOnClickListener {
+            startActivity(Intent(this, Notification_You::class.java))
+            finish()
+        }
 
+        findViewById<RelativeLayout>(R.id.edit_page).setOnClickListener {
+            startActivity(Intent(this, Edit_profile::class.java))
+            finish()
+        }
 
+        findViewById<RelativeLayout>(R.id.friends_highlight).setOnClickListener {
+            startActivity(Intent(this, Highlight::class.java))
+            finish()
+        }
     }
 }
